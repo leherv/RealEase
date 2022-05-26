b400a92eded8b5fa52c6acda80d8e266fb76ec78
Viktor.Leher <viktor.leher@cssteam.at>
2022-05-26 07:47:17 +0200

    Added handling of PlaywrightExceptions in low-level details Reason: scraping should not stop if one ScrapeTarget of one Media throws an exception that could occur due to a website being unavailable or having changed its structure It is important to not handle other exceptions than PlaywrightExceptions on the lowest level because we want to throw in this case so we notice grave mistakes

--- src/Infrastructure.Scraper/Adapters/PlaywrightScraper.cs
+++ src/Infrastructure.Scraper/Adapters/PlaywrightScraper.cs
@@ -1,23 +1,41 @@
 using System.Threading.Tasks;
 using Application.Ports.Scraper;
+using Domain.ApplicationErrors;
 using Domain.Results;
 using Infrastructure.Scraper.Base;
+using Microsoft.Extensions.Logging;
 using Microsoft.Playwright;
 
 namespace Infrastructure.Scraper.Adapters;
 
 public class PlaywrightScraper : IScraper
 {
+    private readonly ILogger<PlaywrightScraper> _logger;
+
+    public PlaywrightScraper(ILogger<PlaywrightScraper> logger)
+    {
+        _logger = logger;
+    }
+
     public async Task<Result<ScrapedMediaRelease>> Scrape(ScrapeInstruction scrapeInstruction)
     {
-        // Program.Main(new[] {"install"});
-        using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync();
-        var page = await browser.NewPageAsync();
-        await page.GotoAsync(scrapeInstruction.ResourceUrl);
+        try
+        {
+            // Program.Main(new[] {"install"});
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync();
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync(scrapeInstruction.ResourceUrl);
 
-        var scrapeStrategy = ReleaseScrapeStrategyFactory.Create(scrapeInstruction.WebsiteName);
-        
-        return await scrapeStrategy.Execute(page, scrapeInstruction);
+            var scrapeStrategy = ReleaseScrapeStrategyFactory.Create(scrapeInstruction.WebsiteName);
+
+            return await scrapeStrategy.Execute(page, scrapeInstruction);
+        }
+        catch (PlaywrightException playwrightException)
+        {
+            _logger.LogWarning(playwrightException,
+                $"Scraping for Media with name {scrapeInstruction.MediaName} and ScrapeTarget with websiteName {scrapeInstruction.WebsiteName} and ResourceUrl {scrapeInstruction.ResourceUrl} failed.");
+            return Result<ScrapedMediaRelease>.Failure(Errors.Scraper.ScrapeFailedError(playwrightException.Message));
+        }
     }
 }
