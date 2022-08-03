14042739fbb2fff3e2abca47778efa8111dae678
Viktor Leher <viktor.leher@gmail.com>
2022-08-03 19:46:33 +0200

    Added link for adding bot to discord server

--- src/Infrastructure.Discord/Settings/DiscordSettings.cs
+++ src/Infrastructure.Discord/Settings/DiscordSettings.cs
@@ -5,4 +5,5 @@ public record DiscordSettings
     public string Token { get; set; }
     public string ClientId { get; set; }
     public string ClientSecret { get; set; }
+    public string AddBotUri { get; set; }
 }
