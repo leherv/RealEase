b621fbe5cdc6d0c3ce1f2203701c74a27bc4cd72
Viktor Leher <viktor.leher@gmail.com>
2022-08-04 10:00:57 +0200

    Make RedirectURI configurable as well (#33)
    * Make RedirectURI configurable as well
    
    * Add RedirectUri to appsettings.json

--- src/ReleaseNotifierApp/Controllers/AuthController.cs
+++ src/ReleaseNotifierApp/Controllers/AuthController.cs
@@ -1,7 +1,9 @@
 ﻿using AspNet.Security.OAuth.Discord;
+using Infrastructure.Discord.Settings;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace ReleaseNotifierApp.Controllers;
 
@@ -10,6 +12,13 @@ namespace ReleaseNotifierApp.Controllers;
 [Route("signin-discord")]
 public class AuthController : Controller
 {
+    private readonly DiscordSettings _discordSettings;
+
+    public AuthController(IOptions<DiscordSettings> discordSettings)
+    {
+        _discordSettings = discordSettings.Value;
+    }
+
     [HttpGet]
     public IActionResult SignInDiscord()
     {
@@ -19,7 +28,7 @@ public class AuthController : Controller
     [HttpGet("[action]")]
     public async Task Login()
     {
-        await HttpContext.ChallengeAsync(DiscordAuthenticationDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = "http://localhost:5000" });
+        await HttpContext.ChallengeAsync(DiscordAuthenticationDefaults.AuthenticationScheme, new AuthenticationProperties { RedirectUri = _discordSettings.RedirectUri });
     }
     
     [HttpGet("[action]")]
