68c9481f26deea204db7d683b8f7d90f99f4a55f
Viktor Leher <viktor.leher@gmail.com>
2022-08-04 09:55:00 +0200

    Make RedirectURI configurable as well

--- src/Infrastructure.Discord/Settings/DiscordSettings.cs
+++ src/Infrastructure.Discord/Settings/DiscordSettings.cs
@@ -6,4 +6,5 @@ public record DiscordSettings
     public string ClientId { get; set; }
     public string ClientSecret { get; set; }
     public string AddBotUri { get; set; }
+    public string RedirectUri { get; set; }
 }
