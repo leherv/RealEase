36375d53e394b056013ff46cd77107e9ca291fae
Viktor Leher <viktor.leher@gmail.com>
2022-08-16 22:42:17 +0200

    Added handling of timeoutException when scraping for media names

--- src/Infrastructure.Scraper/Adapters/PlaywrightMediaNameScraper.cs
+++ src/Infrastructure.Scraper/Adapters/PlaywrightMediaNameScraper.cs
@@ -1,4 +1,6 @@
-﻿using Application.Ports.Scraper;
+﻿using Application.Ports.General;
+using Application.Ports.Scraper;
+using Domain.ApplicationErrors;
 using Domain.Results;
 using Infrastructure.Scraper.Base;
 using Microsoft.Playwright;
@@ -7,15 +9,30 @@ namespace Infrastructure.Scraper.Adapters;
 
 public class PlaywrightMediaNameScraper : IMediaNameScraper
 {
+    private readonly IApplicationLogger _applicationLogger;
+
+    public PlaywrightMediaNameScraper(IApplicationLogger applicationLogger)
+    {
+        _applicationLogger = applicationLogger;
+    }
+
     public async Task<Result<ScrapedMediaName>> ScrapeMediaName(ScrapeMediaNameInstruction scrapeMediaNameInstruction)
     {
-        using var playwright = await Playwright.CreateAsync();
-        await using var browser = await playwright.Chromium.LaunchAsync();
-        var page = await browser.NewPageAsync();
-        await page.GotoAsync(scrapeMediaNameInstruction.ResourceUrl);
+        try
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync();
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync(scrapeMediaNameInstruction.ResourceUrl);
 
-        var mediaNameScrapeStrategy = MediaNameScrapeStrategyFactory.Create(scrapeMediaNameInstruction.WebsiteName);
+            var mediaNameScrapeStrategy = MediaNameScrapeStrategyFactory.Create(scrapeMediaNameInstruction.WebsiteName);
 
-        return await mediaNameScrapeStrategy.Execute(page, scrapeMediaNameInstruction);
+            return await mediaNameScrapeStrategy.Execute(page, scrapeMediaNameInstruction);
+        }
+        catch (TimeoutException timeoutException)
+        {
+            _applicationLogger.LogWarning(timeoutException, $"Scraping for mediaName at url {scrapeMediaNameInstruction.ResourceUrl} and Website {scrapeMediaNameInstruction.WebsiteName} failed.");
+            return Result<ScrapedMediaName>.Failure(Errors.Scraper.ScrapeFailedError(timeoutException.Message));
+        }
     }
 }
