b00db5beda6552514dfba22fe7d4f78caff57fa3
Viktor Leher <viktor.leher@gmail.com>
2022-08-17 21:56:29 +0200

    wip

---
+++ src/RealEaseApp/Pages/ViewComponents/Media/AddMediaViewComponent.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealEaseApp.Pages.ViewComponents.Media;
+
+public class AddMediaViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View("~/Pages/ViewComponents/Media/AddMediaViewComponent.cshtml");
+    }
+}
